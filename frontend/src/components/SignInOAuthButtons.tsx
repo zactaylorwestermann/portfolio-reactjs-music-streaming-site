4d25878b6f31f328d18c8ee461965246cc133db2
gummipainkillers <zactaylorwestermann@gmail.com>
2025-03-03 20:12:03 +1100

    ui update

--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -22,6 +22,7 @@ const SignInOAuthButtons = () => {
       variant={"secondary"}
       className="w-full text-white border-zinc-200 h-11"
     >
+      <img src="/google.png" alt="Google" className="size-5" />
       Continue with Google
     </Button>
   );
